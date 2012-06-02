cfb56c7dbb926fe172992257feb500c13be156ca
Etienne Caron <etienne.caron@gmail.com>
2012-06-02 15:38:02 -0400

    Ongoing work on the model... half done pipe factory.

--- src/com/androidmontreal/rhok/pieces/factory/PipeFactory.java
+++ src/com/androidmontreal/rhok/pieces/factory/PipeFactory.java
@@ -3,8 +3,10 @@ package com.androidmontreal.rhok.pieces.factory;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.androidmontreal.rhok.pieces.Direction;
 import com.androidmontreal.rhok.pieces.Pipe;
 import com.androidmontreal.rhok.pieces.Point;
+import com.androidmontreal.rhok.pieces.Pipe.Type;
 
 public class PipeFactory 
 {
@@ -20,18 +22,21 @@ public class PipeFactory
 	
 	public Pipe create(Point point)
 	{
-		PieceType type = this.getRandomPipeType();
-		
-		Pipe pipe = new Pipe(type.getSprite(), point);
-		pipe.setGates(type.getGates());
+		// FIXME: Boris!
+		Pipe pipe = new Pipe(null,getRandomPipeType(), getRandomDirection());
 		
 		return pipe;
 	}
 	
-	private PieceType getRandomPipeType()
+	private Pipe.Type getRandomPipeType()
 	{
-		int index = (int) ( Math.random() * (this.pieceType.size() + 1));
-		return this.pieceType.get(index);
+		int index = (int) ( Math.random() * (Pipe.Type.values().length + 1));
+		return Pipe.Type.values()[index];
+	}
+	
+	private Direction getRandomDirection() {
+		int index = (int) ( Math.random() * (Direction.values().length + 1));
+		return Direction.values()[index];
 	}
 	
 	public static PipeFactory getInstance()
