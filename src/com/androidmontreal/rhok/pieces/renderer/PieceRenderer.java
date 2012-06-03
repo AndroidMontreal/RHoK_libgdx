5b02f9ce0183d1c35ee5950ef8a4727e0f3861d2
flegare <flegare@gmail.com>
2012-06-02 23:23:10 -0400

    piece renderer

--- src/com/androidmontreal/rhok/pieces/renderer/PieceRenderer.java
+++ src/com/androidmontreal/rhok/pieces/renderer/PieceRenderer.java
@@ -2,18 +2,23 @@ package com.androidmontreal.rhok.pieces.renderer;
 
 import com.androidmontreal.rhok.pieces.Piece;
 import com.androidmontreal.rhok.pieces.Pipe;
-import com.androidmontreal.rhok.pieces.Pump;
 
 public class PieceRenderer {
 	
+	private PipeRenderer renderer;
+
 	public PieceRenderer(Piece p) {
 		
 		if(p instanceof Pipe){
-			new PipeRenderer((Pipe)p).render();
-		}elseif(p instanceof Pump){
-						
+			renderer = new PipeRenderer((Pipe)p);
 		}
 		
+		//TODO: Add more type render
+		
+	}
+		
+	public void render() {
+		renderer.render();
 	}
 
 }
