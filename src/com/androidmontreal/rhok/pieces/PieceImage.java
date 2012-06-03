bbde7a66b2e59b20ca90dfd7f854ad08fd0edb17
Boris Dubois <deus13@gmail.com>
2012-06-03 13:26:09 -0400

    Piece accessor une Pieceimage
    Signed-off-by: Boris Dubois <deus13@gmail.com>

--- src/com/androidmontreal/rhok/pieces/PieceImage.java
+++ src/com/androidmontreal/rhok/pieces/PieceImage.java
@@ -11,6 +11,14 @@ public class PieceImage extends Image{
 		super(texture);
 		this.piece = piece;
 	}
+
+	public Piece getPiece() {
+		return piece;
+	}
+
+	public void setPiece(Piece piece) {
+		this.piece = piece;
+	}
 	
 	
 }
