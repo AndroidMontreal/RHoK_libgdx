8b4f1e016c922568493ec1fa73b7d535a8914532
David <david@echosdedavid.com>
2012-06-03 13:33:06 -0400

    Added inputprocessor

--- src/com/androidmontreal/rhok/pieces/PieceImage.java
+++ src/com/androidmontreal/rhok/pieces/PieceImage.java
@@ -12,5 +12,23 @@ public class PieceImage extends Image{
 		this.piece = piece;
 	}
 	
+	@Override
+	public boolean touchDown(float x, float y, int pointer) {
+		System.out.println(String.format("pieceimage touchdown detect %f,%f", x, y));
+		return true;
+	}
 	
+	@Override
+	public void touchUp(float x, float y, int pointer) {
+		System.out.println(String.format("pieceimage touchUp detect %f,%f", x, y));
+		
+	}
+	
+	@Override
+	public void touchDragged(float x, float y, int pointer) {
+		System.out.println(String.format("pieceimage dragged detect %f,%f", x, y));
+		this.x = x;
+		this.y = y;
+	}
+
 }
